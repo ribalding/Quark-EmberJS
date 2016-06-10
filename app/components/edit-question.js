99ae029d8bc8cee0d818fe12bef49b88e66452df
rh <ryanjharvey88@gmail.com>
2016-06-10 15:51:22 -0700

    Added functionality to edit questions

---
+++ app/components/edit-question.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    editQuestion(question){
+      var params = {
+        user_name: this.get('user_name') ? this.get('user_name') : "",
+        question_text: this.get('question_text') ? this.get('question_text') : "",
+        notes: this.get('notes') ? this.get('notes') : "",
+        answers: "test"
+      };
+      this.sendAction('editQuestion', question, params);
+      console.log(question, params);
+    }
+  }
+});
--- app/routes/question.js
+++ app/routes/question.js
@@ -3,5 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params){
     return this.store.findRecord('question', params.question_id);
+  },
+
+  actions : {
+    editQuestion(question, params){
+      console.log(question);
+      Object.keys(params).forEach(function(key) {
+        if (params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('question', question.id);
+    }
   }
 });
